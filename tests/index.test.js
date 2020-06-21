25f03505534384e31dd49155477130512073f961
Nathan Krasney <natankrasney@gmail.com>
2020-06-21 11:18:08 +0300

    check returned object

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,3 +4,9 @@ const app = require('../src/app')
 test('route / status is 200',async ()=>{
     await request(app).get('/').expect(200);
 })
+
+test('route / get correct object and status code',async ()=>{
+    const response = await request(app).get('/').expect(201);
+
+    expect(response.body).toMatchObject({msg: "hello world"});
+})
