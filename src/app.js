57c6c790e65c869581b124d3726db57021e0b8ca
Nathan Krasney <natankrasney@gmail.com>
2020-06-22 14:52:18 +0300

    add test with post

--- src/app.js
+++ src/app.js
@@ -3,8 +3,18 @@ console.log('app is loading');
 const express = require('express');
 const app = express();
 
+// --- handle json in body
+app.use(express.json());
+
 app.get('/',(req,res)=>{
     res.send({msg : 'hello world'})
+})
+
+app.post('/',(req,res)=>{
+    // do something with the body
+    console.log(req.body);
+    
+    res.status(201).send(req.body);
 })
 
 module.exports = app;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,19 @@
-const request = require('supertest')
-const app = require('../src/app')
+const request = require("supertest");
+const app = require("../src/app");
 
-test('route / status is 200',async ()=>{
-    await request(app).get('/').expect(200);
-})
+test("route / status is 200", async () => {
+  await request(app).get("/").expect(200);
+});
 
-test('route / get correct object and status code',async ()=>{
-    const response = await request(app).get('/').expect(201);
+test("route / get correct object and status code", async () => {
+  const response = await request(app).get("/").expect(200);
 
-    expect(response.body).toMatchObject({msg: "hello world"});
-})
+  expect(response.body).toMatchObject({ msg: "hello world" });
+});
+
+test("route / post correct status code", async () => {
+  const response = await request(app)
+    .post("/")
+    .send({ name: "nathan" })
+    .expect(201);
+});
